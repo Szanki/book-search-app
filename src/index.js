e7c40849aa520047e91ff0a688937d51e5249759
Michał <hortexix@gmail.com>
2020-05-18 10:08:44 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv/config";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import dotenv from "dotenv/config";
 console.log(process.env);
 
 ReactDOM.render(<App />, document.getElementById("root"));
